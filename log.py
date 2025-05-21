8caac9e961dd3e5b893d0c293e22b4fda7a5eb08
alan-shawn <8756550+alan-shawn@users.noreply.github.com>
2025-05-21 16:19:11 +0800

    fix log

--- log.py
+++ log.py
@@ -1,13 +1,33 @@
 import logging
-LOG_FORMAT = "%(levelname)s %(asctime)s %(name)s  %(message)s "
-DATE_FORMAT = '[%Y-%m-%d %H:%M:%S] '
+from logging.handlers import TimedRotatingFileHandler
+import os
 
-logging.basicConfig(level=logging.DEBUG,
-                    format=LOG_FORMAT,
-                    datefmt = DATE_FORMAT ,
-                    filename="./log/test.log"
-                    )
+log_dir = "log"
+os.makedirs(log_dir, exist_ok=True)
+
+log_file = os.path.join(log_dir, "app.log")
+
+handler = TimedRotatingFileHandler(
+    filename=log_file,
+    when='midnight',
+    interval=1,
+    backupCount=10,
+    encoding='utf-8',
+    utc=False
+)
+
+handler.suffix = "%Y-%m-%d"
+
+formatter = logging.Formatter(
+    fmt="[%(levelname)s] %(asctime)s %(message)s",
+    datefmt="[%Y-%m-%d %H:%M:%S]"
+)
+handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 
 def log(msg):
-    logging.info(msg)
+    logger.info(msg)
