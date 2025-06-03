65574085e5a2d3f1eb85b5c1b9ec0a55639b9d13
alan-shawn <8756550+alan-shawn@users.noreply.github.com>
2025-06-03 18:15:26 +0800

    fix

--- agent_service.py
+++ agent_service.py
@@ -77,8 +77,8 @@ def response():
     thread_id = data.get("thread_id")
     log(f"query data: {data},user_input:{query},thread_id:{thread_id}.")
     inputs = {"messages": [("system", "You are an agent that retrieves cryptocurrency data. If the data contains time-series values, "
-                                      "please return the data preferably in JSONC format to facilitate the user in generating charts. "
-                                      "The language of the returned results should match the user's input language."),
+                                      "please return the data preferably in JSONC format. The language of the returned results should "
+                                      "match the user's input language."),
                            ("user", query)]}
     query_response = graph.invoke(inputs,config={"configurable": {"thread_id": thread_id}})
     log(f"Agent response is {query_response}.")
