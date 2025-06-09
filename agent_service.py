776a10d6d1cb7f16b06703a85b5c902fc5979d04
alan-shawn <8756550+alan-shawn@users.noreply.github.com>
2025-06-09 17:46:28 +0800

    add langfuse

--- agent_service.py
+++ agent_service.py
@@ -9,8 +9,11 @@ from langgraph.graph import END, START, StateGraph, MessagesState
 from langgraph.checkpoint.memory import MemorySaver
 from typing import Literal
 import os
+from langfuse.langchain import CallbackHandler
 
 load_dotenv()
+
+langfuse_handler = CallbackHandler()
 
 port = os.environ.get("PORT")
 app = Flask(__name__)
@@ -77,7 +80,7 @@ def response():
     thread_id = data.get("thread_id")
     log(f"query data: {data},user_input:{query},thread_id:{thread_id}.")
     inputs = {"messages": [("user", query)]}
-    query_response = graph.invoke(inputs,config={"configurable": {"thread_id": thread_id}})
+    query_response = graph.invoke(inputs,config={"configurable": {"thread_id": thread_id}, "callbacks": [langfuse_handler]})
     log(f"Agent response is {query_response}.")
     rsp = query_response["messages"][-1].content
     res_completion = {
